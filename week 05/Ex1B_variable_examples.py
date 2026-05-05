9f0096de7afb38bfd51dbbede75541f8dfb744e8
Alondra Martinez <alondramartinez0227@gmail.com>
2026-05-04 23:25:51 -0400

    add Ex1B

---
+++ week 05/Ex1B_variable_examples.py
@@ -0,0 +1,29 @@
+#Example Variables
+
+customer_id = 12345 # could potentially be a string if ID included letters
+customer_name = "Alondra Martinez"
+customer_gender = "Female" # could be stored as "F" or "M" to save space
+customer_date_of_birth = "1996-02-27"
+drivers_license_number = "ABC123456" # kept as a string because it included letters 
+auto_policy_number = "PQLL998365" # string since policy numbers are likely to include both letters and numbers
+
+# variables for my own name and city where I grew up
+
+your_name = "Alondra Martinez"
+city = "Charlotte"
+state = "NC"
+print(your_name,city,state)
+
+# Lab 2 
+# a) What is the full list of reserved words that can’t be used for variable names?
+# False, def, if, raise, None, del, import, return
+# True, elif, in, try, and, else, is, while
+# as, except, lambda, assert, finally, nonlocal, yield, break, for, not
+# class, form, or, continue, global, pass
+
+# if: used to make decision (runs code only if ^something is true^)
+# for: used to reapeat code a certain number of times or ^loop^ through items
+# def: used to create a functon 
+# return: used to send a result back from function 
+# import: used to bring in code from another file or library 
+
