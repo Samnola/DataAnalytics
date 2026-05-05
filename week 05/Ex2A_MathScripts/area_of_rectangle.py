de98302bd144eeea106d314283f0289532eb7a06
Alondra Martinez <alondramartinez0227@gmail.com>
2026-05-05 17:01:07 -0400

    add area_of_rectangle

---
+++ week 05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,8 @@
+# Area of rectangle
+print()
+side_a= 27
+print("Side A is", side_a)
+side_b=2
+print("Side B is", side_b)
+area_of_rectangle= side_a * side_b
+print("Area of rectangle is " + str(area_of_rectangle))
