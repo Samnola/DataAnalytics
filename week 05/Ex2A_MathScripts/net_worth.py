04481196e7de38513b80c768f221503a5299cf94
Alondra Martinez <alondramartinez0227@gmail.com>
2026-05-05 16:14:49 -0400

    add networth

---
+++ week 05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,18 @@
+# Networth calculations 
+print ()
+# Assets
+cash=15000
+savings= 250000
+home= 650000
+total_assets=cash+savings+home 
+print("total assets " + str(total_assets))
+
+# Debts
+school_loan= 55000
+credit_card_debt=2500
+debt= school_loan+credit_card_debt
+print("debt " + str(debt))
+
+# Networth
+networth= total_assets-debt 
+print("Total networth " + str(networth))
