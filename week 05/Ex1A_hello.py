90a256cbba07f91a1df102d3562f7a7280524a42
Alondra Martinez <alondramartinez0227@gmail.com>
2026-05-04 22:49:06 -0400

    add ex1a

--- week 05/Ex1A_hello.py
+++ week 05/Ex1A_hello.py
@@ -1,1 +1,20 @@
 print ('Hello world!')
+
+
+message= 'Hello world!'
+print(message)
+# Hello world is displaying twice because it runs everything at once and ther is no way to run a single query individually. 
+
+
+# Displaying dollars and cents
+
+dollars = 3
+cents = 0.50
+print(dollars+cents)
+# The result showed 3.5 instead of 3.50 because python removed zeros in decimal numbers. 
+cents = cents + .25
+print(dollars+cents)
+
+d_str = "3 dollars"
+c_str = "50 cents"
+print(d_str + " " + c_str)
