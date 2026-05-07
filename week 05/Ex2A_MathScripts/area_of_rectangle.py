7b24594b4d03db4b5407084931cde3a49d3ce630
Alondra Martinez <alondramartinez0227@gmail.com>
2026-05-07 18:40:16 -0400

    add lab_3

--- week 05/Ex2A_MathScripts/area_of_rectangle.py
+++ week 05/Ex2A_MathScripts/area_of_rectangle.py
@@ -6,3 +6,10 @@ side_b=2
 print("Side B is", side_b)
 area_of_rectangle= side_a * side_b
 print("Area of rectangle is " + str(area_of_rectangle))
+
+# using (input)
+
+side_a = int(input("Enter side A: "))
+print("Side A is " + str(side_a))
+# A potential pitfall to doing this would be the user tpying letters when Im expecting a number. 
+# user could also potentially leave blank or use unexpected characters which would display an error. 
