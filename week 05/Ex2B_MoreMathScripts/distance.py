712c739098ddea75c3fbbb6ba48ed763b31ee503
Alondra Martinez <alondramartinez0227@gmail.com>
2026-05-08 17:42:53 -0400

    add distance.py

--- week 05/Ex2B_MoreMathScripts/distance.py
+++ week 05/Ex2B_MoreMathScripts/distance.py
@@ -4,3 +4,11 @@
 # function from the math module.
 
 # use ** to calculate square root (one * is mulitplication)
+
+import math
+
+x1, y1 = 1,2
+x2, y2 = 4,6
+
+distance = math.sqrt((x2 - x1)**2 + (y2 -y1)**2)
+print(distance)
