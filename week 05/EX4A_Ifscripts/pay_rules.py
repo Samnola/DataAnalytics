5cdd2b290cae54924699617b5a18c2b577d89041
Alondra Martinez <alondramartinez0227@gmail.com>
2026-05-10 13:18:00 -0400

    add pay_rules

--- week 05/EX4A_Ifscripts/pay_rules.py
+++ week 05/EX4A_Ifscripts/pay_rules.py
@@ -1,14 +1,16 @@
 # Gross income calculation
 
-pay_rate = 17.30
-hours_worked = 45
+pay_rate = 25.50
+hours_worked = 55
  
-if hours_worked > 40:
-    overtime_hours = hours_worked - 40
-    regular_pay = 40 * pay_rate
+if hours_worked > 40: # check if employee worked more than 40 hours
+    overtime_hours = hours_worked - 40 # calculate overtime hours 
+    regular_pay = 40 * pay_rate # regular pay for first 40 hrs
     overtime_pay = overtime_hours * (pay_rate * 1.5)
     gross_pay = regular_pay + overtime_pay
 else:
-    gross_pay = pay_rate * hours_worked
+    gross_pay = pay_rate * hours_worked # no overtime
 
-    print(gross_pay)
+    
+print(gross_pay)
+
