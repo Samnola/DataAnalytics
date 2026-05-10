10b0fae9c0ebc51091774f060252ff047c1dba8c
Alondra Martinez <alondramartinez0227@gmail.com>
2026-05-10 13:56:04 -0400

    add greeting

---
+++ week 05/EX4A_Ifscripts/greeting.py
@@ -0,0 +1,12 @@
+# greeting
+
+hour = 4
+
+if hour <= 23 or hour < 4: #specific conditions first 
+    print("What are you doing up so late??")
+elif hour < 10: 
+    print("Good morining!")
+elif hour < 17:
+    print("Good day!")
+else:
+    print("Good evening!")
