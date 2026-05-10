c142eebe0c027dba3e6351515badfcf420a2a906
Alondra Martinez <alondramartinez0227@gmail.com>
2026-05-10 12:54:56 -0400

    add ifscripts

---
+++ week 05/EX4A_Ifscripts/if_basics.py
@@ -0,0 +1,13 @@
+# if statements
+
+x = 100
+y = 20
+# == checks if two values are equal 
+# vs = assigns or stores values 
+
+# check if x divided by y equals 5 
+if x / y == 5: # this runs only if condition is true.
+    print("x divided by y is 5") # indented code with if statement 
+    x = 1
+else:  # else runs if condtion is false 
+    print("are you sure that's correct?")
